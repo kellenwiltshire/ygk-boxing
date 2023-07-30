ea7236ae8ba84f128ab4a983f22df5a4598d7d34
RateTheLandlordAdmin <r8thelandlord@gmail.com>
2023-07-30 12:27:22 -0400

    Still trying to sort build error...

--- pages/store/product/[slug].tsx
+++ pages/store/product/[slug].tsx
@@ -1,14 +1,22 @@
 import ProductInfo from "../../../components/Store/ProductInfo";
-import client from "../../../square.connect";
-import { ApiResponse, CatalogObject, ListCatalogResponse, RetrieveCatalogObjectResponse } from "square";
+import {
+  ApiResponse,
+  CatalogObject,
+  Client,
+  Environment,
+  ListCatalogResponse,
+  RetrieveCatalogObjectResponse
+} from "square";
 import { ICatalogObject } from "../../../utils/types/CatalogTypes";
 import JSONBig from "json-bigint";
+import { GetStaticPaths, GetStaticProps } from "next";
 
 interface IProps {
   product: ICatalogObject;
 }
 
 const ProductPage = ({ product }: IProps) => {
+
   return (
     <ProductInfo product={product} />
   );
@@ -16,40 +24,73 @@ const ProductPage = ({ product }: IProps) => {
 };
 export default ProductPage;
 
-export async function getStaticPaths() {
-  const response: ApiResponse<ListCatalogResponse> = await client.catalogApi.listCatalog(undefined, "ITEM");
-  if (response.result.objects) {
-    const products: Array<CatalogObject> = response.result.objects.filter(obj => {
-      if (obj.itemData !== undefined) {
-        return obj;
-      }
-    });
-    const paths = products.map(product => {
-      return {
-        params: {
-          slug: product.id
-        }
-      };
-    });
-    return {
-      paths,
-      fallback: true
-    };
-  }
-}
-
-export async function getStaticProps({ params }) {
+export const getStaticPaths: GetStaticPaths = async () => {
+  const client = new Client({
+    accessToken: process.env.SQUARE_ACCESS_TOKEN,
+    environment: Environment.Production
+  });
   try {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
-    const response: ApiResponse<RetrieveCatalogObjectResponse> = await client.catalogApi.retrieveCatalogObject(params.slug);
-
-    if (response.result.object?.itemData) {
+    const response: ApiResponse<ListCatalogResponse> = await client.catalogApi.listCatalog(undefined, "ITEM");
+    if (response.result.objects) {
+      /* eslint-disable @typescript-eslint/strict-boolean-expressions */
+      const paths: Array<{
+        params: {
+          slug: string
+        }
+      }> = response.result.objects.map(item => {
+        return {
+          params: {
+            slug: item.id
+          }
+        };
+      }) || [];
       return {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
-        props: { product: JSONBig.parse(JSONBig.stringify(response.result.object)) }
+        paths: paths,
+        fallback: true
+      };
+    } else {
+      return {
+        paths: [],
+        fallback: false
       };
     }
-  } catch (e) {
-    throw new Error("Unable to retrieve catalog object");
+  } catch (error) {
+    console.error("Error fetching product paths:", error);
+    return {
+      paths: [],
+      fallback: false
+    };
   }
-}
+};
+
+export const getStaticProps: GetStaticProps<{
+  product: ICatalogObject
+}> = async ({ params }) => {
+  const client = new Client({
+    accessToken: process.env.SQUARE_ACCESS_TOKEN,
+    environment: Environment.Production
+  });
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (!params?.slug) return {
+    notFound: true
+  };
+  try {
+    const response: ApiResponse<RetrieveCatalogObjectResponse> = await client.catalogApi.retrieveCatalogObject(params.slug as string);
+    if (response.result.object?.itemData) {
+      return {
+        props: {
+          product: JSONBig.parse(JSONBig.stringify(response.result.object)) as ICatalogObject
+        }
+      };
+    } else {
+      return {
+        notFound: true
+      };
+    }
+  } catch (error) {
+    console.error("Error fetching product details:", error);
+    return {
+      notFound: true
+    };
+  }
+};

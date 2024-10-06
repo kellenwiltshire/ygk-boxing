9b0be77dbbe9fe74ccdd27b39ad0208fbb458bad
kellenwiltshire <kellenwiltshire@outlook.com>
2024-10-06 08:48:06 -0400

    Update Schedule and Packages

--- components/Schedule/Vars/Events.ts
+++ components/Schedule/Vars/Events.ts
@@ -25,10 +25,6 @@ export const events: Array<IDay> = [
         time: "6:00pm - 6:15pm"
       },
       {
-        name: "Cardio Boxing Workout (Ages 13+)",
-        time: "6:15pm - 7:15pm"
-      },
-      {
         id: 'OPENGYM',
         name: "Open Gym (sparring, pads, stretching, personal workouts)",
         time: "7:15pm - 8:00pm"
@@ -53,7 +49,7 @@ export const events: Array<IDay> = [
         time: "5:30pm - 6:15pm"
       },
       {
-        id:'TADULT',
+        id:'ADULT',
         name: "Adult Boxing Workout/Class (Ages 13+)",
         time: "6:15pm - 7:15pm"
       },
@@ -107,7 +103,7 @@ export const events: Array<IDay> = [
         time: "5:45pm - 6:15pm"
       },
       {
-        id:'TADULT',
+        id:'ADULT',
         name: "Adult Boxing Workout/Class (Ages 13+)",
         time: "6:15pm - 7:15pm"
       },
@@ -156,19 +152,15 @@ export const events: Array<IDay> = [
 export const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 export const classDescriptions = [
-  {
-    id:'MADULT',
-    name: "MONDAY ADULT WORKOUT",
-    description: "Revitalize your Mondays with the ultimate YGK Boxing workout, merging calisthenics and full-body exercises for a transformative fitness experience. Designed for all, this challenging routine promises muscle gains, weight loss, and a confidence boost. Embrace the dynamic fusion of boxing, calisthenics, and intense full-body movements, but be prepared – mastery requires dedication through practice and repetition. This isn't just a workout; it's a journey to unlock your full potential, ensuring that anyone, regardless of their starting point, can thrive and achieve their fitness goals with perseverance. It's not just the best workout in town; it's your path to a stronger, fitter, and more confident you."
-  },
+  
   {
     id:'BEGINNERS',
     name: "BEGINNERS CLASS",
     description: "Embark on a welcoming journey into the world of YGK boxing with our gentle and open beginners class. Tailored for novices, the session emphasizes fundamental elements such as basic footwork, balance, and punch techniques. Led by experienced trainers, this light-hearted introduction ensures a supportive atmosphere, making it the perfect starting point for those eager to grasp the basics of boxing while building confidence and coordination."
   },
   {
-    id:'TADULT',
-    name: "TUESDAY AND THURSDAY ADULT WORKOUT",
+    id:'ADULT',
+    name: "ADULT WORKOUT",
     description: "Step into our inclusive and challenging YGK boxing fitness class, open to participants of all levels. Begin with a dynamic first segment featuring skipping for agility, shadow boxing for technique refinement, and a blend of pushups and calisthenics for overall strength. The second part focuses on honing your boxing skills with bag work, slipping drills, and a deep dive into technical aspects of the sport. This comprehensive workout promises an empowering experience for everyone, whether you're a beginner or an advanced boxer, providing an opportunity to enhance both your fitness and boxing proficiency."
   },
   {

1cec0d241464411ebc3e48e6f710dc3628deb05e
kellenwiltshire <kellenwiltshire@outlook.com>
2025-10-09 16:10:46 -0400

    Update product

--- pages/store/product/[slug].tsx
+++ pages/store/product/[slug].tsx
@@ -1,32 +1,35 @@
-import ProductInfo from "../../../components/Store/ProductInfo";
-import { ICatalogObject } from "../../../utils/types/CatalogTypes";
-import JSONBig from "json-bigint";
-import { GetServerSideProps } from "next";
-import client from "../../../square.connect";
-import { NextParsedUrlQuery } from "next/dist/server/request-meta";
+import ProductInfo from '../../../components/Store/ProductInfo'
+import { ICatalogObject } from '../../../utils/types/CatalogTypes'
+import JSONBig from 'json-bigint'
+import { GetServerSideProps } from 'next'
+import client from '../../../square.connect'
+import { NextParsedUrlQuery } from 'next/dist/server/request-meta'
 
 interface IProps {
-  product: ICatalogObject;
+	product: ICatalogObject
 }
 
 const ProductPage = ({ product }: IProps) => {
-
-  return (
-    <ProductInfo product={product} />
-  );
-
-};
-export default ProductPage;
+	return <ProductInfo product={product} />
+}
+export default ProductPage
 
 interface PageProps {
-  product: ICatalogObject;
+	product: ICatalogObject
 }
 
-export const getServerSideProps: GetServerSideProps<PageProps, NextParsedUrlQuery> = async (context) => {
-  const response = await client.catalogApi.retrieveCatalogObject(context.query.slug as string);
-  return {
-    props: {
-      product: JSONBig.parse(JSONBig.stringify(response.result.object)) as ICatalogObject
-    }
-  };
-};
+export const getServerSideProps: GetServerSideProps<
+	PageProps,
+	NextParsedUrlQuery
+> = async (context) => {
+	const response = await client.catalog.object.get({
+		objectId: context.query.slug as string,
+	})
+	return {
+		props: {
+			product: JSONBig.parse(
+				JSONBig.stringify(response.object),
+			) as ICatalogObject,
+		},
+	}
+}
